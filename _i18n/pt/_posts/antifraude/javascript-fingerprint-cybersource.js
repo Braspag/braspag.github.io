0d67339e2949bbd403246e4f78e96ae37458cf77
Ellen C. Silva <esilva@braspag.com.br>
2022-03-16 11:09:45 -0300

    Update javascript-fingerprint-cybersource.js
    removed space

--- _i18n/pt/_posts/antifraude/javascript-fingerprint-cybersource.js
+++ _i18n/pt/_posts/antifraude/javascript-fingerprint-cybersource.js
@@ -6,6 +6,6 @@
 <!-- BODY -->
 <body>
     <nonscript>
-        <iframe style="width: 100px; height: 100px; border: 0; psition:absolute; top: -5000px;" src="https://h.online-metrix.net/fp/tags?org_id=ProviderOrgId &session_id=ProviderMerchantIdProviderIdentifier"></iframe>
+        <iframe style="width: 100px; height: 100px; border: 0; psition:absolute; top: -5000px;" src="https://h.online-metrix.net/fp/tags?org_id=ProviderOrgId&session_id=ProviderMerchantIdProviderIdentifier"></iframe>
     </nonscript>
 </body>
