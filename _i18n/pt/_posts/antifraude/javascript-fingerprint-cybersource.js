32086531d9d1dafb5ad1106e87c67d21c6117e8b
Ellen C. Silva <esilva@braspag.com.br>
2022-11-29 08:54:54 -0300

    fix - javascript-fingerprint-cybersource.js - tag
    Correção da tag <noscript> no script em JS para fingerprint com a threatmetrix

--- _i18n/pt/_posts/antifraude/javascript-fingerprint-cybersource.js
+++ _i18n/pt/_posts/antifraude/javascript-fingerprint-cybersource.js
@@ -5,7 +5,7 @@
 
 <!-- BODY -->
 <body>
-    <nonscript>
+    <noscript>
         <iframe style="width: 100px; height: 100px; border: 0; psition:absolute; top: -5000px;" src="https://h.online-metrix.net/fp/tags?org_id=ProviderOrgId&session_id=ProviderMerchantIdProviderIdentifier"></iframe>
-    </nonscript>
+    </noscript>
 </body>
